1d6b15f9fdff4dfcc60a206d38c5cfe33bda664a
Madeleine Marcano <madeleine1302@gmail.com>
2018-05-30 15:24:42 -0400

    codigo de encode y decode

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,28 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-  },
-  decode: () => {
-    /* Acá va tu código */
-  }
+    encode: (offset, str) => {
+        offset = document.getElementById("offset").value;
+        str = document.getElementById('str').value;
+        let message = ''; // ir guandando cada caracter
+        for (let i = 0; i < str.length; i++) {
+            const character = str.charCodeAt(i); // guardo el codigo ascii de cada i 
+            if (65 <= character && character <= 90) // para identificar las mayúsculas
+                message += String.fromCharCode((character - 65 + offset) % 26 + 65); //
+            else if (97 <= character && character <= 122) // para identificar las minísculas 
+                message += String.fromCharCode((character - 97 + offset) % 26 + 97);
+            else
+                message += str.charAt(i); //para concatenar las letas
+        }
+        return document.getElementById("encode").addEventListener("click", function() {
+            encode(, );
+        });
+
+        function encode(offset, str) {
+
+            document.getElementById("demo").innerHTML = result;
+        }
+    },
+
+    decode: (offset, string) => {
+        return encode(string, (26 - offset))
+    }
 }
