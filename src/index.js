e2037279b7bdb35aafe6ab3fa7c0ed77db85d8aa
Madeleine Marcano <madeleine1302@gmail.com>
2018-05-30 05:35:34 -0400

    creando funcion displace

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-/* Acá va tu código */
+// /* Acá va tu código */
+// window.index = {
+function displace(text, num) {
+    let message = '';
+    for (let i = 0; i < text.length; i++) {
+        const character = text.charCodeAt(i);
+        if (65 <= character && character <= 90)
+            message = message + String.fromCharCode((character - 65 + num) % 26 + 65);
+        else if (97 <= character && character <= 122)
+            message = message + String.fromCharCode((character - 97 + num) % 26 + 97);
+        else
+            message += text.charAt(i);
+    }
+    return message;
+}
