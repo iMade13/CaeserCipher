e0131d1496e413711a9c7a1fe51bc2fcc59f29e2
Madeleine Marcano <madeleine1302@gmail.com>
2018-05-31 16:01:18 -0400

    probando ramas

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // /* Acá va tu código */ 
 
 document.getElementById("encode").addEventListener("click", () => {
-    let offset = document.getElementById('offset').value;
-    let message = document.getElementById('message').value;
+    const offset = document.getElementById('offset').value;
+    const message = document.getElementById('message').value;
     document.getElementById('result').innerHTML = window.cipher.encode(offset, message);
 });
 document.getElementById("decode").addEventListener("click", () => {
-    let offset = document.getElementById('offset').value;
-    let message = document.getElementById('message').value;
+    const offset = document.getElementById('offset').value;
+    const message = document.getElementById('message').value;
     document.getElementById('result').innerHTML = window.cipher.decode(offset, message);
 });
