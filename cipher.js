0be950625cf9d2171e53b8c780a0b665ee23cc23
Madeleine Marcano <madeleine1302@gmail.com>
2018-06-01 09:33:45 -0400

    nueva formula para el decode

--- cipher.js
+++ cipher.js
@@ -1,21 +1,24 @@
-window.cipher = {
-    encode: (offset, str) => {
+window.cipher = ({
+    encode: (str, offset) => {
         let message = '';
         offset = parseInt(offset); // ir guandando cada caracter
         for (let i = 0; i < str.length; i++) {
-            const character = str.charCodeAt(i); // guardo el codigo ascii de cada i 
-            if (65 <= character && character <= 90) // para identificar las mayúsculas
+            let character = str.charCodeAt(i); // guardo el codigo ascii de cada i 
+            if (65 <= character && character <= 90) {
                 message += String.fromCharCode(((character - 65 + offset) % 26) + 65); //
-            else if (97 <= character && character <= 122) // para identificar las minísculas 
+            } // para identificar las mayúsculas
+            else if (97 <= character && character <= 122) {
                 message += String.fromCharCode(((character - 97 + offset) % 26) + 97);
-            else
+            } // para identificar las minísculas 
+            else {
                 message += str.charAt(i); //para concatenar las letas
+            }
         }
         return message;
     },
 
-    decode: (offset, str) => {
-        return window.cipher.encode((26 - offset), str);
+    decode: (str, offset) => {
+        return cipher.encode(str, (26 - (offset % 26)));
     }
 
-}
+});
